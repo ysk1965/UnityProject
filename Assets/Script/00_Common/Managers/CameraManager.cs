831d8f7fb97e006f5136cbe84a514e0034e04776
sgyoo1 <sgyoo1@cookapps.com>
2021-04-03 23:40:35 +0900

    [FIX]

---
+++ Assets/Script/00_Common/Managers/CameraManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : GameObjectSingleton<CameraManager>
+{
+    public void Shake(float amount = 0.5f, float duration = 0.25f)
+    {
+        if (this.cameraTransform == null) return;
+        Run.Coroutine(this.ShakeCoroutine(amount * 1.5f, duration));
+    }
+
+    public void SetCameraObj(Transform cameraTransform)
+    {
+        this.cameraTransform = cameraTransform;
+    }
+
+    private IEnumerator ShakeCoroutine(float amount, float duration)
+    {
+        this.originPos = this.cameraTransform.localPosition;
+        float timer = 0;
+        while (timer <= duration)
+        {
+            this.cameraTransform.localPosition = (Vector3)Random.insideUnitCircle * amount + this.originPos;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        this.cameraTransform.localPosition = this.originPos;
+    }
+
+    private Transform cameraTransform;
+    private Vector3 originPos;
+}
---
+++ Assets/Script/00_Common/Managers/GameSceneManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Scene
+{
+    Splash,
+    Main,
+    Lobby,
+    InGame,
+}
+
+
+public class GameSceneManager : GameObjectSingleton<GameSceneManager>
+{
+    /////////////////////////////////////////////////////////////
+    // public
+
+    public static string CurrentScene_String { get => UnityEngine.SceneManagement.SceneManager.GetActiveScene().name; }
+    public static Scene CurrentScene;
+
+    public static void Restart()
+    {
+        GameObject.Destroy(DontDestroyObject.Instance.gameObject);
+        Transition.LoadLevel(Scene.Splash.ToString(), 0.2f, Color.black);
+    }
+
+    public static void MoveScene(Scene scene, bool useTransition = true)
+    {
+        CurrentScene = scene;
+        if (useTransition)
+            Transition.LoadLevel(scene.ToString(), 0.2f, Color.black);
+        else
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene.ToString());
+    }
+
+    public static void MoveScene(string sceneName, bool useTransition = true)
+    {
+        if (useTransition)
+            Transition.LoadLevel(sceneName, 0.2f, Color.black);
+        else
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+}
+
